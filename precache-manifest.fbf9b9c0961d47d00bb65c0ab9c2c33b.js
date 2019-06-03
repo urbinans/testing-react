e2a7bdc55309f7e5e7d31495e269368071978b44
Nelson Urbina <urbinans@gmail.com>
2019-06-03 19:59:49 -0400

    Updates

---
+++ precache-manifest.fbf9b9c0961d47d00bb65c0ab9c2c33b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d46a4f948c751676456417fad49ed0a",
+    "url": "/testing-react/index.html"
+  },
+  {
+    "revision": "cdbce58d1e2d7329edc3",
+    "url": "/testing-react/static/css/main.773b7e0e.chunk.css"
+  },
+  {
+    "revision": "358c83931b06f25d2c73",
+    "url": "/testing-react/static/js/2.e7e0944a.chunk.js"
+  },
+  {
+    "revision": "cdbce58d1e2d7329edc3",
+    "url": "/testing-react/static/js/main.6afce373.chunk.js"
+  },
+  {
+    "revision": "3acb858a2fd2bdb5a07f",
+    "url": "/testing-react/static/js/runtime~main.f58d18e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testing-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
